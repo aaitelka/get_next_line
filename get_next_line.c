97ab928f056e90da8872071dc831c88ddb48817e
aaitelka <aaitelka@student.1337.ma>
2024-01-18 16:34:19 +0100

    save

--- get_next_line.c
+++ get_next_line.c
@@ -33,50 +33,64 @@ char *read_line(char *buf) {
         return (NULL);
     len = endl_index(buf);
     if (len == 0)
-        return (free(buf),NULL);
+        return (free(buf), NULL);
     line = (char *) malloc(sizeof(char) * (len + 1));
     if (!line)
         return (free(buf), NULL);
     line[len] = '\0';
-    while (len)
-    {
-        len--;
+    while (len--)
         line[len] = buf[len];
-    }
-    return (line);
+    return (free(buf), line);
 }
+
+char *new_rem(const char *rem)
+{
+    char *new_rem;
+    size_t mlen;
+    size_t len;
+
+    if (!rem)
+        return (NULL);
+    rem = strchr(rem, 10) + 1;
+    len = endl_index(rem);
+    mlen = len;
+    new_rem = (char *) malloc(sizeof(char) * len);
+    if (!new_rem)
+        return (NULL);
+    while (*rem && len--)
+        *new_rem++ = *rem++;
+    *new_rem = '\0';
+    new_rem -= mlen - 1;
+    return (new_rem);
+}
+
 
 char *get_next_line(int fd) {
     static char *rem;
-    char *line;
-    char *rline = NULL;
     char *buf;
     ssize_t ret;
 
-    line = NULL;
-    buf = (char *) malloc(sizeof(char) * (size_t)(BUFFER_SIZE + 1));
+    buf = (char *) malloc(sizeof(char) * (size_t) (BUFFER_SIZE + 1));
     if (!buf)
         return (NULL);
     if (fd < 0 || read(fd, NULL, 0) < 0 || BUFFER_SIZE <= 0)
         return (free(buf), NULL);
-    if (rem)
-        line = rem;
     ret = read(fd, buf, BUFFER_SIZE);
-    while (*buf || *line) {
+    while (*buf || *rem) {
         buf[ret] = '\0';
         if (ft_strchr(buf, '\n'))
             rem = ft_strchr(buf, '\n') + 1;
-        else if (ft_strchr(line, '\n'))
-            rem = ft_strchr(line, '\n') + 1;
-        line = join(line, buf);
-        if (ft_strchr(line, '\n') || (!ret && !ft_strchr(line, '\n')))
+        else if (ft_strchr(rem, '\n'))
         {
-            rline = read_line(line);
-            free(line);
-            break;
+            new_rem(rem);
+//            break;
+        }
+        rem = join(rem, buf);
+        if (ft_strchr(rem, '\n') || ft_strchr(buf, 10) || (!ret && !ft_strchr(rem, '\n'))) {
+            return (free(buf), read_line(rem));
         }
         ret = read(fd, buf, BUFFER_SIZE);
     }
     free(buf);
-    return (rline);
+    return (read_line(rem));
 }
