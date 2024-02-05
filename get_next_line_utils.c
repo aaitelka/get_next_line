7e6c1afe3338c9f248e8ecc27d7d5755abbba99a
aaitelka <aaitelka@student.1337.ma>
2024-02-05 17:30:45 +0100

    Ze3LouuuK

--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -12,51 +12,51 @@
 
 #include "get_next_line.h"
 
-size_t ft_strlen(const char *s) {
+size_t ft_strlen(char *s)
+{
     size_t len;
 
     if (!s)
         return (0);
     len = 0;
-    while (s[len])
+    while (s[len] != '\0')
         len++;
     return (len);
 }
 
-char *ft_strchr(const char *s, int c) {
+char *ft_strchr(char *s, int c)
+{
     if (!s)
         return (NULL);
-    while (*s) {
-        if (*s == (char) c)
-            return ((char *) s);
+    while (*s && *s != (char)c)
         s++;
-    }
-    if (!(char) c)
-        return ((char *) s);
+    if (*s == (char)c)
+        return (char *)s;
     return (NULL);
 }
 
-char *join(char const *s1, char const *s2) {
+char *join(char *s1, char *s2)
+{
     char *str;
     size_t s1len;
     size_t s2len;
-    size_t dstlen;
+    size_t i;
 
-    if (!s1 || !s2)
+    if (!s1 && !s2)
         return (NULL);
+    if (!s1)
+        return (s2);
     s1len = ft_strlen(s1);
     s2len = ft_strlen(s2);
-    dstlen = s1len + s2len;
-    str = (char *) malloc(sizeof(char) * (size_t)(dstlen + 1));
+    str = (char *) malloc(sizeof(char) * (s1len + s2len + 1));
     if (!str)
         return (NULL);
-    while ((*s1 || *s2) && dstlen--) {
-        if (*s1)
-            *str++ = *s1++;
-        else
-            *str++ = *s2++;
-    }
-    *str = '\0';
-    str -= (s1len + s2len);
+    i = -1;
+    while (s1[++i])
+        str[i] = s1[i];
+    int     j = -1;
+    while (s2[++j])
+        str[i++] = s2[j];
+    str[i] = '\0';
     return (str);
 }
